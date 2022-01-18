ab3aea6ff334af4b4763a72bc78a8b123d41e72c
Matthew Schlegel <mkschleg@gmail.com>
2022-01-18 16:55:50 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/ExampleExperiment.jl
@@ -0,0 +1,5 @@
+module ExampleExperiment
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ExampleExperiment
+using Test
+
+@testset "ExampleExperiment.jl" begin
+    # Write your tests here.
+end
